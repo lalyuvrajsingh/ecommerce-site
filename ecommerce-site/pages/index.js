7cff1278bb11c0ab90c6cbaf643f631047e361cc
lalyuvrajsingh <lysingh24@gmail.com>
2023-05-14 16:11:26 +0530

    Heading towards main page

--- ecommerce-site/pages/index.js
+++ ecommerce-site/pages/index.js
@@ -1,5 +1,6 @@
 import HeroBanner from '@/components/HeroBanner'
 import { Inter } from '@next/font/google'
+import Wrapper from '@/components/Wrapper'
 
 
 const inter = Inter({ subsets: ['latin'] })
@@ -9,8 +10,14 @@ export default function Home() {
     <>
 
 
-      <main className='text-3xl h-[2000px]'>
+      <main className=''>
         <HeroBanner/>
+        <Wrapper>
+          <div className=' text-center max-w-[800px] mx-auto my-[50px] md:my-[80px]'>
+            <h1>HEading</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium natus recusandae sunt dolor aliquid unde, enim voluptate aliquam dolores delectus?</p>
+          </div>
+        </Wrapper>
       </main>
 
     </>
