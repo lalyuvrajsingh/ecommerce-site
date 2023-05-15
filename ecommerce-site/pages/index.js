72551fee169a44cd365825d58f803841d8691beb
lalyuvrajsingh <lysingh24@gmail.com>
2023-05-15 05:52:55 +0530

    Main Page Done!!
    Done with the main page, super happy with the design that came out be. I personally don't like it, also I'm a fanboy of Apple.

--- ecommerce-site/pages/index.js
+++ ecommerce-site/pages/index.js
@@ -1,6 +1,7 @@
 import HeroBanner from '@/components/HeroBanner'
 import { Inter } from '@next/font/google'
 import Wrapper from '@/components/Wrapper'
+import ProductCard from '@/components/ProductCard'
 
 
 const inter = Inter({ subsets: ['latin'] })
@@ -14,10 +15,22 @@ export default function Home() {
         <HeroBanner/>
         <Wrapper>
           <div className=' text-center max-w-[800px] mx-auto my-[50px] md:my-[80px]'>
-            <h1>HEading</h1>
+            <h1 className='text-[30px] my-5 font-semibold'>Welcome to the Futuristic Store.</h1>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium natus recusandae sunt dolor aliquid unde, enim voluptate aliquam dolores delectus?</p>
           </div>
         </Wrapper>
+        
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 px-5 my-14 mx-auto md:px-0 ">
+          <ProductCard/>
+          <ProductCard/>
+          <ProductCard/>
+          <ProductCard/>
+          <ProductCard/>
+          <ProductCard/>
+          <ProductCard/>
+          <ProductCard/>
+        </div>
+        
       </main>
 
     </>
