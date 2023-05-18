c2e33877eb9d3f37f063db59839125dcb251f070
lalyuvrajsingh <lysingh24@gmail.com>
2023-05-18 20:23:11 +0530

    connecting the database
    it took so long to connect the postgresql database! I almost lost the hope and was thinking to make a different layout for managing the orders. The issues came when I had node js v19 but the strapi was supporting 18 and below. Then I learned nvm, it is a damn nice thing. Finally end of the day the database got connected, hopefully from tomorrow I'll be working on logics and adding content.

--- ecommerce-site/frontend/pages/index.js
+++ ecommerce-site/frontend/pages/index.js
@@ -2,13 +2,11 @@ import HeroBanner from '@/components/HeroBanner'
 import { Inter } from '@next/font/google'
 import Wrapper from '@/components/Wrapper'
 import ProductCard from '@/components/ProductCard'
-import client from '../backend/lib/client'; // Replace with the correct path to your sanity client file
-import product from '@/backend/schemas/product';
 
 
 const inter = Inter({ subsets: ['latin'] })
 
-export default function Home({products}) {
+export default function Home() {
 
   return (
     <>
@@ -16,7 +14,7 @@ export default function Home({products}) {
         <HeroBanner/>
         <Wrapper>
           <div className=' text-center max-w-[800px] mx-auto my-[50px] md:my-[80px]'>
-            <h1 className='text-[30px] my-5 font-semibold'>{product.title[0]}</h1>
+            <h1 className='text-[30px] my-5 font-semibold'>Welcome</h1>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium natus recusandae sunt dolor aliquid unde, enim voluptate aliquam dolores delectus?</p>
           </div>
         </Wrapper>
@@ -24,9 +22,7 @@ export default function Home({products}) {
 
         {/************************* Product Grid *********************************/}
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 px-5 my-14 mx-auto md:px-0 ">
-        {products?.map((product) => {
-          product.name
-        })}
+        
           <ProductCard/>
           <ProductCard/>
           <ProductCard/>
@@ -47,23 +43,6 @@ export default function Home({products}) {
 }
 
 
-
-
-export async function handler(req, res) {
-  try {
-    const data = await client.fetch('*[_type == "product"]');
-    const products = await client.fetch(query)
-    res.status(200).json(data);
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: 'An error occurred' });
-  }
-
-  return{
-    props: {products}
-  }
-
-}
 
 
 
