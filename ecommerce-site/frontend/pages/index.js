f5883ab0e5e94cc4367361325ddcc0d6fd56bbce
lalyuvrajsingh <lysingh24@gmail.com>
2023-05-19 21:15:48 +0530

    finally data fetched!
    Learned a lot today about how api testing works and how to fetch data, what are the benefits of next js in seo and all.... it was an insightful coding today!

--- ecommerce-site/frontend/pages/index.js
+++ ecommerce-site/frontend/pages/index.js
@@ -2,11 +2,15 @@ import HeroBanner from '@/components/HeroBanner'
 import { Inter } from '@next/font/google'
 import Wrapper from '@/components/Wrapper'
 import ProductCard from '@/components/ProductCard'
+import { useEffect, useState } from 'react'
+import { fetchDataFromAPI } from '@/utils/api'
 
 
-const inter = Inter({ subsets: ['latin'] })
 
-export default function Home() {
+
+
+export default function Home({products}) {
+
 
   return (
     <>
@@ -14,7 +18,7 @@ export default function Home() {
         <HeroBanner/>
         <Wrapper>
           <div className=' text-center max-w-[800px] mx-auto my-[50px] md:my-[80px]'>
-            <h1 className='text-[30px] my-5 font-semibold'>Welcome</h1>
+            <h1 className='text-[30px] my-5 font-semibold'>{products.data?.[0]?.attributes?.name}</h1>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium natus recusandae sunt dolor aliquid unde, enim voluptate aliquam dolores delectus?</p>
           </div>
         </Wrapper>
@@ -22,6 +26,10 @@ export default function Home() {
 
         {/************************* Product Grid *********************************/}
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 px-5 my-14 mx-auto md:px-0 ">
+
+        {products.data.map((product)=>(
+          <ProductCard key={product.id} data={product}/>
+        ))}
         
           <ProductCard/>
           <ProductCard/>
@@ -40,6 +48,16 @@ export default function Home() {
     </>
 
   )
+}
+
+
+export async function getStaticProps(){
+
+  const products = await fetchDataFromAPI("/api/products?populate=*")
+
+  return{
+    props: {products}
+  }
 }
 
 
